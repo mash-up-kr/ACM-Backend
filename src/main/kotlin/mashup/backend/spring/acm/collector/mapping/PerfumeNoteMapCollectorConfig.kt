b3033cacb361bc4551c4b739193afa2110032de6
Haeseong Jeon <seong0428@gmail.com>
2021-09-07 01:57:14 +0900

    feat: 향수 노트 매핑 코드 추가

---
+++ src/main/kotlin/mashup/backend/spring/acm/collector/mapping/PerfumeNoteMapCollectorConfig.kt
@@ -0,0 +1,43 @@
+package mashup.backend.spring.acm.collector.mapping
+
+import mashup.backend.spring.acm.BatchConfig.Companion.SPRING_BATCH_JOB_NAMES
+import mashup.backend.spring.acm.collector.note.NoteCollectorTasklet
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@ConditionalOnProperty(value = [SPRING_BATCH_JOB_NAMES], havingValue = "perfumeNoteMapCollectorJob")
+@Configuration
+class PerfumeNoteMapCollectorConfig {
+    @Autowired
+    lateinit var jobBuilderFactory: JobBuilderFactory
+    @Autowired
+    lateinit var jobRepository: JobRepository
+    @Autowired
+    lateinit var stepBuilderFactory: StepBuilderFactory
+
+    @Bean
+    fun perfumeNoteMapCollectorJob(): Job {
+        return jobBuilderFactory[JOB_NAME]
+            .repository(jobRepository)
+            .start(
+                stepBuilderFactory["perfumeNoteMapCollectorStep"]
+                    .tasklet(perfumeNoteMapCollectorTasklet())
+                    .build()
+            )
+            .build()
+    }
+
+    @Bean
+    fun perfumeNoteMapCollectorTasklet(): Tasklet = PerfumeNoteMapCollectorTasklet()
+
+    companion object {
+        const val JOB_NAME = "perfumeNoteMapCollectorJob"
+    }
+}
