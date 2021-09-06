a4ec72af8a1f09636f46491d00d21c73715d2bb5
Haeseong Jeon <seong0428@gmail.com>
2021-09-07 00:14:54 +0900

    config: spring batch 설정 추가

---
+++ src/main/kotlin/mashup/backend/spring/acm/BatchConfig.kt
@@ -0,0 +1,8 @@
+package mashup.backend.spring.acm
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.context.annotation.Configuration
+
+@EnableBatchProcessing
+@Configuration
+class BatchConfig
