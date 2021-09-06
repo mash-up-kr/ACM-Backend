1078ca779ddbd9163eed78b41db38a6e31bb6e9f
Haeseong Jeon <seong0428@gmail.com>
2021-09-07 00:16:29 +0900

    feat: Jsoup 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation ("org.jsoup:jsoup:1.14.2")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
