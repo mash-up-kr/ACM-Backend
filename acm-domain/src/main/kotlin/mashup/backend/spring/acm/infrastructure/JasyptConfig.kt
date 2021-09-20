4294dc2bec4ba8f2dfcf26fe6b6adba8c8cfe089
Haeseong Jeon <seong0428@gmail.com>
2021-09-20 21:36:47 +0900

    feat: 암호화 라이브러리 추가

---
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/infrastructure/JasyptConfig.kt
@@ -0,0 +1,9 @@
+package mashup.backend.spring.acm.infrastructure
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableEncryptableProperties
+class JasyptConfig {
+}
