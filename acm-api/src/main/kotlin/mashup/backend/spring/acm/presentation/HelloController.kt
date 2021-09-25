d569983a47d556eebebdd092da9b83a989282e17
Haeseong Jeon <seong0428@gmail.com>
2021-09-25 13:46:16 +0900

    feat: health check api 추가

---
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/HelloController.kt
@@ -0,0 +1,10 @@
+package mashup.backend.spring.acm.presentation
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping
+    fun hello(): String = "Hello, acm-api"
+}
