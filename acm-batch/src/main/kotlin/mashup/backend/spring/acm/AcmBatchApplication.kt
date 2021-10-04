eee3b1e6d46d07bb3bc38b49cbdb715db5c7c126
Haeseong Jeon <seong0428@gmail.com>
2021-10-04 16:25:52 +0900

    feat: 배치 실행 시작시간 추가

--- acm-batch/src/main/kotlin/mashup/backend/spring/acm/AcmBatchApplication.kt
+++ acm-batch/src/main/kotlin/mashup/backend/spring/acm/AcmBatchApplication.kt
@@ -2,11 +2,13 @@ package mashup.backend.spring.acm
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.time.LocalDateTime
 
 @SpringBootApplication
 class AcmBatchApplication
 
 fun main(args: Array<String>) {
 	System.setProperty("spring.config.name", "application,batch")
-	runApplication<AcmBatchApplication>(*args)
+	val argsWithStartTime = args + " startTime=${LocalDateTime.now()}"
+	runApplication<AcmBatchApplication>(*argsWithStartTime)
 }
