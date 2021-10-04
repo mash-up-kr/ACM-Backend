add4401a17ee42458e223c11e6f57109174a5571
Haeseong Jeon <seong0428@gmail.com>
2021-10-04 17:14:10 +0900

    feat: 향수 url 절대경로로 보정

--- acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/scrap/perfume_url/PerfumeUrlScrapingJobService.kt
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/scrap/perfume_url/PerfumeUrlScrapingJobService.kt
@@ -10,10 +10,15 @@ class PerfumeUrlScrapingJobService(
 ) {
     @Transactional
     fun createIfNotExists(perfumeUrl: String) : Pair<Boolean, PerfumeUrlScrapingJob?> {
-        if (perfumeUrlRepository.existsByUrl(perfumeUrl)) {
+        val url = if (perfumeUrl.startsWith("https://www.fragrantica.com/")) {
+            perfumeUrl
+        } else {
+            "https://www.fragrantica.com$perfumeUrl"
+        }
+        if (perfumeUrlRepository.existsByUrl(url)) {
             return Pair(false, null)
         }
-        val perfumeUrlScrapingJob = perfumeUrlRepository.save(PerfumeUrlScrapingJob(url = perfumeUrl))
+        val perfumeUrlScrapingJob = perfumeUrlRepository.save(PerfumeUrlScrapingJob(url))
         return Pair(true, perfumeUrlScrapingJob)
     }
 }
