68c9dcefebf8672e5ff892a95556ab82bfc4a6c3
YJeongKim <yjeongisme@gmail.com>
2021-10-05 02:26:52 +0900

    fix: import path 수정

--- acm-batch/src/main/kotlin/mashup/backend/spring/acm/collector/perfume/PerfumeCollectorConfig.kt
+++ acm-batch/src/main/kotlin/mashup/backend/spring/acm/collector/perfume/PerfumeCollectorConfig.kt
@@ -1,6 +1,6 @@
 package mashup.backend.spring.acm.collector.perfume
 
-import mashup.backend.spring.acm.infrastructure.BatchConfig
+import mashup.backend.spring.acm.infrastructure.BatchConfig.Companion.SPRING_BATCH_JOB_NAMES
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
--- acm-batch/src/main/kotlin/mashup/backend/spring/acm/infrastructure/BatchConfig.kt
+++ acm-batch/src/main/kotlin/mashup/backend/spring/acm/infrastructure/BatchConfig.kt
@@ -3,7 +3,6 @@ package mashup.backend.spring.acm.infrastructure
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.context.annotation.Configuration
 
-
 @EnableBatchProcessing
 @Configuration
 class BatchConfig {
