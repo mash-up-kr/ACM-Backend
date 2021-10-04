0fcc18b2b0ef8e4bb7064ae65140aba87e50fcb3
Haeseong Jeon <seong0428@gmail.com>
2021-10-05 00:23:53 +0900

    test: 테스트 실패 해결

--- acm-domain/src/test/kotlin/mashup/backend/spring/acm/TestConfiguration.kt
+++ acm-domain/src/test/kotlin/mashup/backend/spring/acm/TestConfiguration.kt
@@ -1,7 +1,24 @@
 package mashup.backend.spring.acm
 
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers
+import org.springframework.context.annotation.Bean
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.transaction.PlatformTransactionManager
 
 @SpringBootApplication
 class TestConfiguration {
+    @Bean
+    fun transactionManager(
+        transactionManagerCustomizers: ObjectProvider<TransactionManagerCustomizers>
+    ): PlatformTransactionManager {
+        val transactionManager = JpaTransactionManager()
+        transactionManagerCustomizers.ifAvailable { customizers: TransactionManagerCustomizers ->
+            customizers.customize(
+                transactionManager
+            )
+        }
+        return transactionManager
+    }
 }
