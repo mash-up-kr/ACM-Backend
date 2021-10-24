ccb0f8ea3ee78e8a9a2b27bf978beb4224261609
전해성 <seong0428@gmail.com>
2021-10-24 17:31:48 +0900

    노트 그룹 추가 (#58)
    * feat: 노트 그룹이 이미 존재하면 새로 생성하지않음
    
    * feat: 로그 추가
    
    * feat: bean override 설정 추가

--- acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/note/NoteGroupRepository.kt
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/note/NoteGroupRepository.kt
@@ -3,4 +3,5 @@ package mashup.backend.spring.acm.domain.note
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface NoteGroupRepository : JpaRepository<NoteGroup, Long> {
+    fun existsByOriginalName(originalName: String): Boolean
 }
--- acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/note/NoteGroupService.kt
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/note/NoteGroupService.kt
@@ -14,6 +14,9 @@ class NoteGroupServiceImpl(
 ): NoteGroupService {
     @Transactional(readOnly = false)
     override fun create(noteGroupCreateVo: NoteGroupCreateVo): NoteGroup {
+        if (noteGroupRepository.existsByOriginalName(noteGroupCreateVo.name)) {
+            throw RuntimeException("노트그룹이 이미 존재합니다. name: ${noteGroupCreateVo.name}") // TODO: BusinessException
+        }
         return noteGroupRepository.save(NoteGroup.from(noteGroupCreateVo))
     }
 }
