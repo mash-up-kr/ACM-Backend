0987fbf608f611f57f02c39e4af23740155c5852
Haeseong Jeon <seong0428@gmail.com>
2021-10-30 02:02:27 +0900

    feat: 향수, 브랜드 검색 API 추가

--- acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/perfume/PerfumeData.kt
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/perfume/PerfumeData.kt
@@ -89,3 +89,16 @@ data class SimplePerfumeNote(
         )
     }
 }
+
+data class PerfumeSearchRequest(
+    val name: String,
+)
+
+data class PerfumeSearchResponse(
+    val perfumes: List<PerfumeSimpleResponse>,
+)
+
+data class PerfumeSimpleResponse(
+    val id: Long,
+    val name: String,
+)
