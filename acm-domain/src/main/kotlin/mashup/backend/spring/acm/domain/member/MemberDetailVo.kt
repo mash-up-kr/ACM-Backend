bc82fc77117bbca3df6afc120e7e990a09b47cf6
Haeseong Jeon <seong0428@gmail.com>
2021-10-31 18:56:55 +0900

    fix: 컴파일 실패 해결

--- acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/member/MemberDetailVo.kt
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/member/MemberDetailVo.kt
@@ -1,12 +1,10 @@
 package mashup.backend.spring.acm.domain.member
 
-import mashup.backend.spring.acm.domain.perfume.Gender
-
 data class MemberDetailVo(
     val id: Long,
     val status: MemberStatus,
     val name: String?,
-    val gender: Gender?,
+    val gender: MemberGender?,
     val ageGroup: AgeGroup?,
 ) {
     constructor(member: Member) : this(
