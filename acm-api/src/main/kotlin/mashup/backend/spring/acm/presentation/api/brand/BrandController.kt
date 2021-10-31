b6fb24cf237b175d9b81a38b355288e1f68bebf3
Haeseong Jeon <seong0428@gmail.com>
2021-10-31 18:25:02 +0900

    feat: endpoint 추가

--- acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/brand/BrandController.kt
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/brand/BrandController.kt
@@ -1,14 +1,16 @@
 package mashup.backend.spring.acm.presentation.api.brand
 
 import mashup.backend.spring.acm.presentation.ApiResponse
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/brands")
 class BrandController {
+    @GetMapping("/{brandId}")
+    fun getBrand(
+        @PathVariable brandId: Long
+    ): ApiResponse<BrandDetailData> = TODO()
+
     @PostMapping("/search")
     fun search(
         @RequestBody brandSearchRequest: BrandSearchRequest,
