b9126f79cd3e7ddce1d05d64410a7d5ac054ffd7
Haeseong Jeon <seong0428@gmail.com>
2021-11-01 00:34:55 +0900

    feat: 인기브랜드 조회 기능 추가

--- acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/brand/BrandService.kt
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/brand/BrandService.kt
@@ -9,6 +9,7 @@ interface BrandService {
     fun create(brandCreateVo: BrandCreateVo): Brand
     fun rename(brandId: Long, name: String)
     fun findAll(): List<Brand>
+    fun getPopularBrands(): List<Brand>
 }
 
 @Service
@@ -33,4 +34,33 @@ class BrandServiceImpl(
         ?: throw RuntimeException("브랜드를 찾을 수 없습니다. brandId: $brandId")
 
     override fun findAll(): List<Brand> = brandRepository.findAll()
+
+    override fun getPopularBrands(): List<Brand> = POPULAR_BRAND_URL_LIST.mapNotNull { brandRepository.findByUrl(it) }
+
+    companion object {
+        val POPULAR_BRAND_URL_LIST: List<String> = listOf(
+            // 바이레도, BYREDO
+            "https://www.fragrantica.com/designers/Byredo.html",
+            // 랑방, LANVIN
+            "https://www.fragrantica.com/designers/Lanvin.html",
+            // 조말론, Jo Malone London
+            "https://www.fragrantica.com/designers/Jo-Malone-London.html",
+            // 딥티크, Diptyque
+            "https://www.fragrantica.com/designers/Diptyque.html",
+            // 디올, DIOR
+            "https://www.fragrantica.com/designers/Dior.html",
+            // 샤넬, CHANEL
+            "https://www.fragrantica.com/designers/Chanel.html",
+            // 불가리, bulgari, BVLGARI
+            "https://www.fragrantica.com/designers/Bvlgari.html",
+            // 입생로랑, Yves Saint Laurent
+            "https://www.fragrantica.com/designers/Yves-Saint-Laurent.html",
+            // 클린, CLEAN
+            "https://www.fragrantica.com/designers/Clean.html",
+            // 존바바토스, John Varvatos
+            "https://www.fragrantica.com/designers/John-Varvatos.html",
+            // 버버리, Burberry
+            "https://www.fragrantica.com/designers/Burberry.html",
+        )
+    }
 }
