203894109c37cd97aadade713365e209c86b0dfc
전해성 <seong0428@gmail.com>
2021-11-08 19:43:49 +0900

    recommender pseudo code 추가 (#124)
    * feat: pseudo code 추가
    
    * feat: pseudo code 추가
    
    * fix: size 계산 로직 수정

---
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/recommend/RecommendRequestVo.kt
@@ -0,0 +1,9 @@
+package mashup.backend.spring.acm.domain.recommend
+
+import mashup.backend.spring.acm.domain.member.MemberDetailVo
+
+data class RecommendRequestVo(
+    val memberDetailVo: MemberDetailVo?,
+    val size: Int?,
+    val exceptIds: Set<Long>?,
+)
