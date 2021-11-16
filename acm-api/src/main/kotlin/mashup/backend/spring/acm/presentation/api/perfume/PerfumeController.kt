d43c537ec3265a5a186e8cfcef1b6409a1e7aa5b
Haeseong Jeon <seong0428@gmail.com>
2021-11-17 00:26:08 +0900

    docs: perfume api 설명 추가

--- acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/perfume/PerfumeController.kt
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/perfume/PerfumeController.kt
@@ -1,11 +1,16 @@
 package mashup.backend.spring.acm.presentation.api.perfume
 
+import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import mashup.backend.spring.acm.domain.perfume.PerfumeService
 import mashup.backend.spring.acm.presentation.ApiResponse
 import mashup.backend.spring.acm.presentation.assembler.toPerfumeDetail
 import org.springframework.web.bind.annotation.*
 
+@Api(
+    description = "향수",
+    tags = ["perfume"],
+)
 @RequestMapping("/api/v1/perfumes")
 @RestController
 class PerfumeController(val perfumeService: PerfumeService) {
