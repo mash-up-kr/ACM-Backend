2f6b2142893eee7db0cb4ff3dd4f670079950c56
Haeseong Jeon <seong0428@gmail.com>
2021-11-17 00:30:12 +0900

    docs: brand api 설명 추가

--- acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/brand/BrandController.kt
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/brand/BrandController.kt
@@ -1,15 +1,27 @@
 package mashup.backend.spring.acm.presentation.api.brand
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import mashup.backend.spring.acm.application.brand.BrandApplicationService
 import mashup.backend.spring.acm.presentation.ApiResponse
 import mashup.backend.spring.acm.presentation.assembler.toDto
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@Api(
+    description = "브랜드",
+    tags = ["brand"],
+)
 @RestController
 @RequestMapping("/api/v1/brands")
 class BrandController(
     private val brandApplicationService: BrandApplicationService,
 ) {
+    @ApiOperation(
+        value = "브랜드 상세 조회",
+    )
     @GetMapping("/{brandId}")
     fun getBrand(
         @PathVariable brandId: Long,
