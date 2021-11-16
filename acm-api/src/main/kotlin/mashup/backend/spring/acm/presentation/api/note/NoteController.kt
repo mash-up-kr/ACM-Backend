0084b6e5492800d5f2a7329a999be95797ecd474
Haeseong Jeon <seong0428@gmail.com>
2021-11-17 00:24:08 +0900

    docs: note api 설명 추가

--- acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/note/NoteController.kt
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/note/NoteController.kt
@@ -1,19 +1,25 @@
 package mashup.backend.spring.acm.presentation.api.note
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import mashup.backend.spring.acm.application.note.NoteApplicationService
 import mashup.backend.spring.acm.presentation.ApiResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Api(
+    description = "노트",
+    tags = ["note"],
+)
 @RestController
 @RequestMapping("/api/v1/notes")
 class NoteController(
     private val noteApplicationService: NoteApplicationService,
 ) {
-    /**
-     * 노트 상세 조회
-     */
+    @ApiOperation(
+        value = "노트 상세 조회",
+    )
     @GetMapping("/{noteId}")
     fun getNoteDetail(noteId: Long): ApiResponse<NoteDetailData> {
         return ApiResponse.success(
