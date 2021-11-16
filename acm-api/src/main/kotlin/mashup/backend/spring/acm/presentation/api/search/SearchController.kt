e9cae1b6e9c218b4bcad979d8a5f7d3c1c671675
Haeseong Jeon <seong0428@gmail.com>
2021-11-17 00:15:40 +0900

    docs: search api 설명 추가

--- acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/search/SearchController.kt
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/search/SearchController.kt
@@ -1,5 +1,7 @@
 package mashup.backend.spring.acm.presentation.api.search
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import mashup.backend.spring.acm.application.search.SearchApplicationService
 import mashup.backend.spring.acm.presentation.ApiResponse
 import mashup.backend.spring.acm.presentation.assembler.toDto
@@ -9,11 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
+@Api(
+    description = "검색",
+    tags = ["search"],
+)
 @RestController
 @RequestMapping("/api/v1/search")
 class SearchController(
     private val searchApplicationService: SearchApplicationService,
 ) {
+    @ApiOperation(
+        value = "이름으로 향수, 브랜드 통합 검색",
+        notes = "brands, perfumes 각각 30개만 결과에 포함됨",
+    )
     @PostMapping
     fun search(
         @RequestBody @Valid searchRequest: SearchRequest,
