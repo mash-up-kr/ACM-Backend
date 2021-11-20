b2fd0bfc65533264cab02f5afc30666d4a0e4342
choiminseong <choi-minseong@naverz-corp.com>
2021-11-21 01:21:47 +0900

    fix: runner에 @profile 추가

--- acm-api/src/main/kotlin/mashup/backend/spring/acm/infrastructure/scheduler/SchedulerRunner.kt
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/infrastructure/scheduler/SchedulerRunner.kt
@@ -4,8 +4,10 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
+@Profile("local", "develop")
 @Component
 class SchedulerRunner(private val schedulers: List<Scheduler>): ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
