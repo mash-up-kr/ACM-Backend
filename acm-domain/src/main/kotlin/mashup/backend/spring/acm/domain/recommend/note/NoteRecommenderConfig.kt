50e51d1c41b29cf1ff974cd404ba19d5dc485007
choiminseong <choi-minseong@naverz-corp.com>
2021-11-21 16:40:54 +0900

    fix: recommender config 수정

--- acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/recommend/note/NoteRecommenderConfig.kt
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/recommend/note/NoteRecommenderConfig.kt
@@ -1,25 +1,21 @@
 package mashup.backend.spring.acm.domain.recommend.note
 
 import mashup.backend.spring.acm.domain.note.Note
-import mashup.backend.spring.acm.domain.note.NoteGroupService
 import mashup.backend.spring.acm.domain.recommend.RecommenderBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class NoteRecommenderConfig(private val noteGroupService: NoteGroupService) {
+class NoteRecommenderConfig(
+    private val recommendNotesByNoteGroupIdsService: RecommendNotesByNoteGroupIdsService,
+    private val recommendNotesByDefaultService: RecommendNotesByDefaultService,
+) {
 
     @Bean
     fun notesByNoteGroupIdsRecommender() = RecommenderBuilder<Note>()
         .recommendService(listOf(
-            recommendNotesByNoteGroupIdsService(),
-            recommendNotesByDefaultService(),
+            recommendNotesByNoteGroupIdsService,
+            recommendNotesByDefaultService,
         ))
         .build()
-
-    @Bean
-    fun recommendNotesByNoteGroupIdsService() = RecommendNotesByNoteGroupIdsService(noteGroupService)
-
-    @Bean
-    fun recommendNotesByDefaultService() = RecommendNotesByDefaultService(noteGroupService)
 }
--- acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/recommend/perfume/PerfumeRecommenderConfig.kt
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/recommend/perfume/PerfumeRecommenderConfig.kt
@@ -1,30 +1,35 @@
 package mashup.backend.spring.acm.domain.recommend.perfume
 
-import mashup.backend.spring.acm.domain.note.NoteGroupService
 import mashup.backend.spring.acm.domain.perfume.Perfume
-import mashup.backend.spring.acm.domain.perfume.PerfumeService
 import mashup.backend.spring.acm.domain.recommend.RecommenderBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class PerfumeRecommenderConfig(
-    private val noteGroupService: NoteGroupService,
-    private val perfumeService: PerfumeService,
+    private val recommendPerfumesByNoteGroupIdsAndGenderAndAgeService: RecommendPerfumesByNoteGroupIdsAndGenderAndAgeService,
+    private val recommendPerfumesByNoteGroupIdsAndGenderService: RecommendPerfumesByNoteGroupIdsAndGenderService,
+    private val recommendPerfumesByGenderService: RecommendPerfumesByGenderService,
+    private val recommendPerfumesByUnisexGenderService: RecommendPerfumesByUnisexGenderService,
+    private val recommendPerfumesByPopularNoteGroupService: RecommendPerfumesByPopularNoteGroupService,
+    private val recommendMonthlyPerfumesService: RecommendMonthlyPerfumesService,
+    private val recommendPerfumesForPresentService: RecommendPerfumesForPresentService,
+    private val recommendPerfumesByDefaultService: RecommendPerfumesByDefaultService
 ) {
-    @Bean
-    fun similarPerfumesRecommender() = RecommenderBuilder<Perfume>()
-        .recommendService(listOf(
-            recommendPerfumesByNoteGroupIdsAndGenderService()
-        ))
-        .build()
+
+@Bean
+fun similarPerfumesRecommender() = RecommenderBuilder<Perfume>()
+    .recommendService(listOf(
+        recommendPerfumesByNoteGroupIdsAndGenderService
+    ))
+    .build()
 
     @Bean
     fun perfumesByGenderRecommender() = RecommenderBuilder<Perfume>()
         .recommendService(listOf(
-            recommendPerfumesByNoteGroupIdsAndGenderService(),
-            recommendPerfumesByGenderService(),
-            recommendPerfumesByUnisexGenderService(),
+            recommendPerfumesByNoteGroupIdsAndGenderService,
+            recommendPerfumesByGenderService,
+            recommendPerfumesByUnisexGenderService,
         ))
         .build()
 
@@ -32,8 +37,8 @@ class PerfumeRecommenderConfig(
     fun popularPerfumesRecommender() = RecommenderBuilder<Perfume>()
         .recommendService(listOf(
             // TODO : 최근 보관함에 많이 담긴 향수
-            recommendPerfumesByPopularNoteGroupService(),
-            recommendMonthlyPerfumesService(),
+            recommendPerfumesByPopularNoteGroupService,
+            recommendMonthlyPerfumesService,
         ))
         .build()
 
@@ -41,12 +46,12 @@ class PerfumeRecommenderConfig(
     fun perfumesByOnboardRecommender() = RecommenderBuilder<Perfume>()
         .recommendService(listOf(
             // todo : digging 개발에 따라 추가 추천 로직 개발해야 함
-            recommendPerfumesByNoteGroupIdsAndGenderAndAgeService(),
-            recommendPerfumesByNoteGroupIdsAndGenderService(),
-            recommendPerfumesByGenderService(),
-            recommendPerfumesByUnisexGenderService(),
-            recommendPerfumesByPopularNoteGroupService(),
-            recommendMonthlyPerfumesService(),
+            recommendPerfumesByNoteGroupIdsAndGenderAndAgeService,
+            recommendPerfumesByNoteGroupIdsAndGenderService,
+            recommendPerfumesByGenderService,
+            recommendPerfumesByUnisexGenderService,
+            recommendPerfumesByPopularNoteGroupService,
+            recommendMonthlyPerfumesService,
         ))
         .build()
 
@@ -54,34 +59,10 @@ class PerfumeRecommenderConfig(
     fun perfumesByNoteGroupRecommender() = RecommenderBuilder<Perfume>()
         .recommendService(listOf(
             // TODO : 디깅 개발하면서 추가 개발해야 함.
-            recommendPerfumesByNoteGroupIdsAndGenderAndAgeService(),
-            recommendPerfumesByNoteGroupIdsAndGenderService(),
-            recommendPerfumesForPresentService(),
+            recommendPerfumesByNoteGroupIdsAndGenderAndAgeService,
+            recommendPerfumesByNoteGroupIdsAndGenderService,
+            recommendPerfumesForPresentService,
         ))
         .build()
-
-    @Bean
-    fun recommendPerfumesByNoteGroupIdsAndGenderAndAgeService() = RecommendPerfumesByNoteGroupIdsAndGenderAndAgeService(noteGroupService, perfumeService)
-
-    @Bean
-    fun recommendPerfumesByNoteGroupIdsAndGenderService() = RecommendPerfumesByNoteGroupIdsAndGenderService(noteGroupService, perfumeService)
-
-    @Bean
-    fun recommendPerfumesByGenderService() = RecommendPerfumesByGenderService(perfumeService)
-
-    @Bean
-    fun recommendPerfumesByUnisexGenderService() = RecommendPerfumesByUnisexGenderService(perfumeService)
-
-    @Bean
-    fun recommendPerfumesByPopularNoteGroupService() = RecommendPerfumesByPopularNoteGroupService(noteGroupService, perfumeService)
-
-    @Bean
-    fun recommendMonthlyPerfumesService() = RecommendPerfumesByPopularNoteGroupService(noteGroupService, perfumeService)
-
-    @Bean
-    fun recommendPerfumesForPresentService() = RecommendPerfumesByPopularNoteGroupService(noteGroupService, perfumeService)
-
-    @Bean
-    fun recommendPerfumesByDefaultService() = RecommendPerfumesByDefaultService(perfumeService)
 
 }
