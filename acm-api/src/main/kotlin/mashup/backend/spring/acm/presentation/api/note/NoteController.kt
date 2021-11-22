020834a56bb0b36ae949614cbb52d4761fad6fa9
Haeseong Jeon <seong0428@gmail.com>
2021-11-22 23:21:58 +0900

    fix: PathVariable 어노테이션 추가

--- acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/note/NoteController.kt
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/note/NoteController.kt
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation
 import mashup.backend.spring.acm.application.note.NoteApplicationService
 import mashup.backend.spring.acm.presentation.ApiResponse
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -21,7 +22,7 @@ class NoteController(
         value = "노트 상세 조회",
     )
     @GetMapping("/{noteId}")
-    fun getNoteDetail(noteId: Long): ApiResponse<NoteDetailData> {
+    fun getNoteDetail(@PathVariable noteId: Long): ApiResponse<NoteDetailData> {
         return ApiResponse.success(
             data = NoteDetailData(
                 note = noteApplicationService.getNote(noteId = noteId),
