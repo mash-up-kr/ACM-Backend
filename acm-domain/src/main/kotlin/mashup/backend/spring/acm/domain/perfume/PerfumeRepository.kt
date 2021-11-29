77879b75225dfecb5665b35032d623cbafe7d411
전해성 <seong0428@gmail.com>
2021-11-29 23:51:18 +0900

    feat: entity graph 추가 (#157)

--- acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/perfume/PerfumeRepository.kt
+++ acm-domain/src/main/kotlin/mashup/backend/spring/acm/domain/perfume/PerfumeRepository.kt
@@ -3,10 +3,13 @@ package mashup.backend.spring.acm.domain.perfume
 import mashup.backend.spring.acm.domain.brand.Brand
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface PerfumeRepository : JpaRepository<Perfume, Long> {
+    @EntityGraph("perfume.brand")
+    override fun findAll(pageable: Pageable): Page<Perfume>
     fun existsByUrl(url: String): Boolean
     fun findByUrl(url: String): Perfume?
     fun findPerfumeById(id: Long): Perfume?
@@ -16,7 +19,10 @@ interface PerfumeRepository : JpaRepository<Perfume, Long> {
     fun findByBrand_IdOrderByRandom(brandId: Long, pageable: Pageable): List<Perfume>
     fun findByNameContaining(name: String, pageable: Pageable): List<Perfume>
     fun findByBrand(brand: Brand): List<Perfume>
+    @EntityGraph("perfume.brand")
     fun findByBrand_id(brandId: Long, pageable: Pageable): Page<Perfume>
+    @EntityGraph("perfume.brand")
     fun findByNotes_note_id(noteId: Long, pageable: Pageable): Page<Perfume>
+    @EntityGraph("perfume.brand")
     fun findByBrand_idAndNotes_note_id(brandId: Long, noteId: Long, pageable: Pageable): Page<Perfume>
 }
