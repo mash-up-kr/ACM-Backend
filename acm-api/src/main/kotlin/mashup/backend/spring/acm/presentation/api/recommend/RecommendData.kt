373011d10dfba6fdeb4293c254a092d0debbe678
Haeseong Jeon <seong0428@gmail.com>
2021-12-13 23:59:47 +0900

    feat: 추천 향수 필드 이름 변경

--- acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/recommend/RecommendData.kt
+++ acm-api/src/main/kotlin/mashup/backend/spring/acm/presentation/api/recommend/RecommendData.kt
@@ -1,44 +1,48 @@
 package mashup.backend.spring.acm.presentation.api.recommend
 
 data class MainRecommendData(
-    val mainRecommend: MainRecommend
+    val mainRecommend: MainRecommend,
 )
 
 data class MainRecommend(
     val title: String,
     val popularBrands: List<PopularBrand>,
     val recommendPerfumes: List<SimpleRecommendPerfumes>,
-    val recommendNoteGroups: List<RecommendNoteGroup>
+    val recommendNoteGroups: List<RecommendNoteGroup>,
 )
 
 data class SimpleRecommendPerfumes(
     var no: Int = -1,
     val title: String,
-    val perfumes: List<SimpleRecommendPerfume>
+    val perfumes: List<SimpleRecommendPerfume>,
 )
 
 data class RecommendNoteGroup(
     val id: Long,
     val name: String,
-    val recommendNotes: List<RecommendNote>
+    val recommendNotes: List<RecommendNote>,
 )
 
 data class RecommendNote(
     val id: Long,
     val name: String,
-    val recommendPerfumes: List<SimpleRecommendPerfume>
+    val recommendPerfumes: List<SimpleRecommendPerfume>,
 )
 
 data class PopularBrand(
     val id: Long,
     val name: String,
     val image: String,
-    val recommendPerfumes: List<SimpleRecommendPerfume>
+    val recommendPerfumes: List<SimpleRecommendPerfume>,
 )
 
 data class SimpleRecommendPerfume(
     val id: Long,
+    @Deprecated("Use 'thumbnailImageUrl' instead")
     val image: String,
+    val thumbnailImageUrl: String,
+    @Deprecated("Use 'brandName' instead")
     val brand: String,
-    val name: String
+    val brandName: String,
+    val name: String,
 )
